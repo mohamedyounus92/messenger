94103dcfea1ad608dd7164cd526516b03e8005ce
Mohamed Younus <younus.hakkim@gmail.com>
2019-04-09 00:20:09 +0530

    Adding BeanParam

--- src/main/java/org/younus/javaworkshop/messenger/resources/MessageResource.java
+++ src/main/java/org/younus/javaworkshop/messenger/resources/MessageResource.java
@@ -2,6 +2,7 @@ package org.younus.javaworkshop.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -10,10 +11,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.younus.javaworkshop.messenger.model.Message;
+import org.younus.javaworkshop.messenger.resources.beans.MessageFilterBean;
 import org.younus.javaworkshop.messenger.service.MessageService;
 
 @Path("/messages")
@@ -23,13 +24,11 @@ public class MessageResource {
 	private MessageService msgService = new MessageService();
 
 	@GET
-	public List<Message> getMessages(@QueryParam("year") int year,
-									 @QueryParam("start") int start,
-									 @QueryParam("size") int size) {
-		if(year != 0)
-			return msgService.getAllMessagesByYear(year);
-		else if(start > 0 && size > 0)
-			return msgService.getAllMessagesWithPagination(start-1, size-1);
+	public List<Message> getMessages(@BeanParam MessageFilterBean filterBean) {
+		if(filterBean.getYear() != 0)
+			return msgService.getAllMessagesByYear(filterBean.getYear());
+		else if(filterBean.getStart() > 0 && filterBean.getSize() > 0)
+			return msgService.getAllMessagesWithPagination(filterBean.getStart()-1, filterBean.getSize()-1);
 		return msgService.getAllMessages();
 	}
 	
